65f1f7b80bcc031af3280e0a6a0263ebbb9d3925
Rojit Manandhar <rojit.1000@gmail.com>
2024-03-08 15:47:02 -0500

    add roc analysis

--- source/analytics/urls.py
+++ source/analytics/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import get_historical_data, Analysis
+from .views import get_historical_data, Analysis, CandleChart
 
 urlpatterns = [
     path('<int:pk>/', get_historical_data, name="get_historical_data"),
-    path('<int:pk>/chart/', Analysis.as_view(), name="analysis"),
+    path('<int:pk>/candle-chart/', CandleChart.as_view(), name="candle-chart"),
+    path('<int:pk>/analytical-chart/', Analysis.as_view(), name="analytical-chart"),
+
 ]
--- source/analytics/views.py
+++ source/analytics/views.py
@@ -4,6 +4,9 @@ from companies.models import Company, DailyData
 from django.views import View
 import time
 import json
+from ta.volatility import BollingerBands
+from ta.momentum import ROCIndicator
+import pandas as pd
 
 def get_historical_data(request, pk):
     company = get_object_or_404(Company, pk=pk)
@@ -13,12 +16,44 @@ def get_historical_data(request, pk):
 
 class Analysis(View):
 
+    def roc_indicator(self, data):
+        df = pd.DataFrame(data)
+        roc = ROCIndicator(df['close'])
+
+        return list(roc.roc())
+
 
     def get(self, request, pk):
         data_from = request.GET.get('from', None)
         data_to = request.GET.get('to')
         if not data_from:
-            data_from = datetime.datetime.now().date() - datetime.timedelta(days=100)
+            data_from = datetime.datetime.now().date() - datetime.timedelta(days=30)
+        
+        if not data_to:
+            data_to = datetime.datetime.now().date()
+
+        company = get_object_or_404(Company, pk=pk)
+        daily_data = DailyData.objects.filter(company=company, date__gt=data_from).values('date', 'open', 'close')
+        roc = self.roc_indicator(daily_data)
+        for i in range(len(daily_data)):
+            daily_data[i]['roc'] = roc[i] if str(roc[i]) != 'nan' else None
+
+        context =  {
+            'data': daily_data
+        }
+        return render(request, 'analytics/default_chart.html', context=context)
+
+
+
+
+
+class CandleChart(View):
+
+    def get(self, request, pk):
+        data_from = request.GET.get('from', None)
+        data_to = request.GET.get('to')
+        if not data_from:
+            data_from = datetime.datetime.now().date() - datetime.timedelta(days=30)
         
         if not data_to:
             data_to = datetime.datetime.now().date()
@@ -37,25 +72,8 @@ class Analysis(View):
         json_data = json.dumps(list(chart_data))
         print(json_data)
         context =  {
-            'data': json_data
+            'data': json_data,
+            'company': company
         }
         return render(request, 'analytics/candle_chart.html', context=context)
-
-    # def get(self, request, pk):
-    #     data_from = request.GET.get('from', None)
-    #     data_to = request.GET.get('to')
-    #     if not data_from:
-    #         data_from = datetime.datetime.now().date() - datetime.timedelta(days=30)
-        
-    #     if not data_to:
-    #         data_to = datetime.datetime.now().date()
-
-    #     company = get_object_or_404(Company, pk=pk)
-    #     daily_data = DailyData.objects.filter(company=company, date__gt=data_from).values('date', 'close', 'open')
-    #     print(daily_data)
-    #     context =  {
-    #         'data': daily_data
-    #     }
-    #     return render(request, 'analytics/default_chart.html', context=context)
-
-
+    
